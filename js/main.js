a87446d0913f78ccb87d4f2fe567eb5c6cc801d1
UKI0x <ono.oooo.syty@gmail.com>
2021-08-19 15:40:01 +0900

    add データ型まで

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-		console.log('Hello Wolrd from main.js!');
+	console.log('it\'s,me!');
+	console.log("it's,me!");
+	console.log("hel\nlo wo\trld");
+	console.log("hello" + "world");
+
+
+	console.log(10 ** 3);
+
+	let price = 500;
+
+	console.log(price);
+
+	price += 100;
+
+	price *=2;
+
+	price ++;
+	price --;
+	console.log(price);
+
+	console.log(typeof 'hello');
+	console.log(typeof 5);
+	console.log(typeof true);
+	console.log(typeof undefined);
+	console.log(typeof null);
