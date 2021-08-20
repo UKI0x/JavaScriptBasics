e3a53916ef4027b6c80b424c64fc8e26712708b8
UKI0x <ono.oooo.syty@gmail.com>
2021-08-21 06:16:35 +0900

    add スコープ

--- js/main.js
+++ js/main.js
@@ -1,40 +1,15 @@
 'use strict';
 
-	function showAd(message = 'Ad') {
-		console.log("---------------");
-		console.log(`------${message}-------`);
-		console.log("---------------");
+// スコープ
+	const x = 2;
+	// ブロックの中（｛｝）で定義された定数や変数はブロック内でしか使用されない＝スコープが効いているから
+	function f() {
+		// const x = 1;
+		console.log(x);
 	}
 
-	showAd();
-	console.log("test");
-	showAd();
-	console.log("test");
 
-	// 関数でまとめると変更に強くなる。同じ処理をまとめて、同じコードを複数回書かなくても良くなる
+	
+	f();
 
-
-	showAd("Header Ad");
-	showAd();
-	showAd("Footer Ad");
-
-
-	// function sum(a, b, c) {
-	// 	// console.log(a + b + c);
-	// 	return a + b + c;
-	// }
-
-	// // sum(1,2,3);
-	// // sum(3,4,5);
-
-	// const total = sum(1,2,3) + sum(3,4,5);
-	// console.log(total);
-
-	// 関数式
-	const sum = function(a, b, c) {
-		// console.log(a + b + c);
-		return a + b + c;
-	};
-
-	const total = sum(1,2,3) + sum(3,4,5);
-	console.log(total);
+	console.log(x);
