6e29b17e7ba218aa57ea4a23d2fa5b734a164401
UKI0x <ono.oooo.syty@gmail.com>
2021-08-21 05:32:44 +0900

    add 関数宣言、関数式

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
 'use strict';
 
-	// let hp = 100;
-
-	// while (hp > 0) {
-	// 	console.log(`${hp} HP left!`);
-	// 	hp -= 15;
-	// }
-
-	let hp = -50;
-
-	do {
-		console.log(`${hp} HP left!`);
-		hp -= 15;
-
-	}while (hp > 0) 
-
-
-	for (let i =1;i <= 10; i ++){
-		if (i === 4) {
-			break;
-		}
-			console.log(i);
-			
+	function showAd(message = 'Ad') {
+		console.log("---------------");
+		console.log(`------${message}-------`);
+		console.log("---------------");
 	}
 
+	showAd();
+	console.log("test");
+	showAd();
+	console.log("test");
 
+	// 関数でまとめると変更に強くなる。同じ処理をまとめて、同じコードを複数回書かなくても良くなる
+
+
+	showAd("Header Ad");
+	showAd();
+	showAd("Footer Ad");
+
+
+	// function sum(a, b, c) {
+	// 	// console.log(a + b + c);
+	// 	return a + b + c;
+	// }
+
+	// // sum(1,2,3);
+	// // sum(3,4,5);
+
+	// const total = sum(1,2,3) + sum(3,4,5);
+	// console.log(total);
+
+	// 関数式
+	const sum = function(a, b, c) {
+		// console.log(a + b + c);
+		return a + b + c;
+	};
+
+	const total = sum(1,2,3) + sum(3,4,5);
+	console.log(total);
