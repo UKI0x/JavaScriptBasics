fa3db76c361668138f9d5c00850536755fe29c06
UKI0x <ono.oooo.syty@gmail.com>
2021-08-21 06:20:13 +0900

    add スコープの分割

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 'use strict';
 
-// スコープ
-	const x = 2;
-	// ブロックの中（｛｝）で定義された定数や変数はブロック内でしか使用されない＝スコープが効いているから
-	function f() {
-		// const x = 1;
-		console.log(x);
-	}
 
 
-	
-	f();
 
+{
+	const x = 100;
 	console.log(x);
+}
