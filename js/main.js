82e8807f04de09c9354ec519054189dfaf6fad5b
UKI0x <ono.oooo.syty@gmail.com>
2021-08-23 04:09:19 +0900

    add 配列　行頭行末削除・追加メソッドについて

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
 'use strict';
 
 {
+
+
+	const scores = [80,90,40,70];
+	scores.push(60,50);
+	scores.shift();//削除は1つずつしかできない
+	// 90,40,70,60,50
+	console.log(scores);
 	
-	// const sccore1 = 80;
-	// const sccore2 = 90;
-	// const sccore3 = 40;
-
-	const scores = [80,90,40];
-	console.log(scores);
-	// 配列の要素1つだけを出力する
-	console.log(scores[1]);
-
-	// 定数でも、配列の要素は変更できる
-	scores[2] = 44;
-	console.log(scores);
-
-	// 配列の要素数を出力　要素名.length
-	console.log(scores.length);
-
-	// console.log(`Score: ${scores[0]}`);
-	// console.log(`Score: ${scores[1]}`);
-	// console.log(`Score: ${scores[2]}`);
-	// ただし、上記の書き方では要素数が1000個とかになると出力が辛いのでループ文にさせる
-
-	// for(let i =0; i < 3;i++){
 	for(let i =0; i < scores.length;i++){
 		console.log(`Score ${i}: ${scores[i]}`);
 	}
