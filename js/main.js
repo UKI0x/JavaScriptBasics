8c0a588738605c11bbf2e51ea73aab273b0a8a53
UKI0x <ono.oooo.syty@gmail.com>
2021-08-24 16:45:15 +0900

    add forEach

--- js/main.js
+++ js/main.js
@@ -1,30 +1,12 @@
 'use strict';
 
 {
-
-	// 分割代入
-	// 配列の中身を別々の定数にしたい場合
+	// forEach()
 	const scores = [80,90,40,70];
 	
-	// const [a,b,c,d] = scores;
-	// console.log(a);
-	// console.log(b);
-	// console.log(c);
-	// console.log(d);
-
-	// 定数に入れたいのは最初の2つだけで、後の2つは配列のままで良かった場合
-	// レスト構文：分割代入と一緒に使うと、先に割り当てた以外の要素を指定した名前の配列に格納してくれる
-	// const [a,b, ...others] = scores;
-	// console.log(a);
-	// console.log(b);
-	// console.log(others);
-
-	// 値の交換
-	let x = 30;
-	let y = 70;
-	[x,y] = [y,x];
-	console.log(x);
-	console.log(y);
-
+	// scores.forEach((score) =>{
+	scores.forEach((score, index) =>{
+		console.log(`Score ${index}: ${score}`)
+	});
 }
 
