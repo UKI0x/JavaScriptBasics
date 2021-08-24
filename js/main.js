a841958a88ea29da2e6884ad75e920bafd5ec878
UKI0x <ono.oooo.syty@gmail.com>
2021-08-24 16:51:11 +0900

    add map

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 'use strict';
 
 {
-	// forEach()
-	const scores = [80,90,40,70];
-	
-	// scores.forEach((score) =>{
-	scores.forEach((score, index) =>{
-		console.log(`Score ${index}: ${score}`)
-	});
+	// map 配列に何らかの処理をして、その結果を別の配列として取得したい場合
+	const prices = [180,190,200];
+
+	// それぞれ20円値上げした結果を配列で出力したい
+	// const updatedPrices = prices.map((price)=>{
+	// 	return price + 20;
+
+	const updatedPrices = prices.map(price=> price + 20);
+	console.log(updatedPrices);
 }
 
