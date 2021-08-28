625a4450bc117dcf01468808cde380baacd13802
UKI0x <ono.oooo.syty@gmail.com>
2021-08-28 23:53:33 +0900

    add 変数の代入

--- js/main.js
+++ js/main.js
@@ -2,28 +2,14 @@
 
 
 {
-	// オブジェクトのプロパティを列挙したいときに、forEachがオブジェクトには使えないので、いくつかの手順を踏む必要がある
-
-	const point = {
-		x: 100,
-		y: 180,
-	};
-
-
-	// Object.keys(point)とすると、pointのすべてのキーを配列で取得できるので、それをkeysという定数にまずは入れる
-	// そうすると、キーが配列で取得できて、配列にはforEach()が使えるので、それを使っていく
-	const keys = Object.keys(point);
-
-	console.log(point);
-	console.log(keys);
-	keys.forEach(key => {
-		console.log(`key : ${key} Value: ${point[key]}`);
-	});
-
-	const points = [
-		{x:30, y: 20},
-		{x:10, y: 50},
-		{x:40, y: 40},
-	];
-	console.log(points[1].y);
+	// let x = 1;
+	// let y = x;
+	// x = 5;
+	// console.log(x);  //5
+	// console.log(y); //1
+	let x = [1,2];
+	let y = x;
+	x[0] = 5;
+	console.log(x);  //[5,2]
+	console.log(y); //[1,2]ではなく[5,2]になる
 }
