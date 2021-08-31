41cb89cac260aa23898019296ebe0cee6f5ee932
UKI0x <ono.oooo.syty@gmail.com>
2021-08-31 23:59:27 +0900

    リセット

--- js/main.js
+++ js/main.js
@@ -2,34 +2,7 @@
 
 
 {
-	// 数値の合計と平均を求める
-	const scores = [10,3,9];
-
-	let sum = 0;
-
-	scores.forEach(score => {
-		sum += score;
-	});
-
-	const avg = sum / scores.length;
-	console.log(sum);
-	console.log(avg);
-
-	// 小数点以下切り捨て
-	console.log(Math.floor(avg));
-
-	// 小数点以下切り上げ
-	console.log(Math.ceil(avg));
-
-
-	// 四捨五入
-	console.log(Math.round(avg));
-
-	// 指定した桁数になるように数値を丸めたい場合
-	console.log(avg.toFixed(3));
-
-	// 乱数を生成する 0以上1未満
-	console.log(Math.random());
+	
 
 
 }
