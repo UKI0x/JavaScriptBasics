53ba47212ac455de845fa06b517bfb9538ff3bcb
UKI0x <ono.oooo.syty@gmail.com>
2021-09-01 16:45:06 +0900

    add setTimeout() 　指定した時間の後に1回だけ処理を実行するように予約する命令

--- js/main.js
+++ js/main.js
@@ -2,21 +2,17 @@
 
 
 {
-	// 警告や確認のダイアログを表示する方法
-
-	// 警告 alertの引数に表示したい文字列を渡してあげる
-	// お知らせを出すとき等に便利
-	alert("hello");
-
-	// ユーザーに確認を求める confirm alertと違い選択肢（キャンセル・OK）がある
-	const answer = confirm("削除しますか?");
-
-	console.log(answer);
-
-	// confirmで出された確認ダイアログでどちらのボタンを押されたかでtrue/false（ブール値）が返ってくるのでif文でその後の処理を分ける
-	if(answer) {
-		console.log("削除しました");
-	}else {
-		console.log("キャンセルしました");
+	let i =0;
+	function showTime() {
+		console.log(new Date());
+		const timeoutId = setTimeout(showTime, 1000);
+		i ++;
+		if (i >2) {
+			clearTimeout(timeoutId);
+		}
 	}
+
+	// 指定した時間のあとに１回だけ処理を実行するように予約する命令
+
+	showTime();
 }
