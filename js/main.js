db405252e761077cf5cbda55870583f32eb5edd8
UKI0x <ono.oooo.syty@gmail.com>
2021-09-02 00:28:02 +0900

    add クラスを拡張したい場合を考える

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//静的メソッドを使ってみよう
+//クラスを拡張したい場合を考える
 {
 	class Post {
 		constructor(text) {
@@ -17,12 +17,23 @@
 				this.show();
 			}
 
-			// 静的メソッド　インスタンスを介さずに直接クラスから呼び出すメソッドを定義する
-			// オブジェクトのインスタンスとは関係がないけど、投稿に関する機能を作成したい場合に便利
-			// プロパティやメソッド違い、静的メソッドはthisが使用できない
-			// クラス内で使われるthisはクラスから作られるインスタンスのため、静的メソッドではインスタンスを作らずに呼び出す為thisは使用できない
-			static showInfo() {
-				console.log("post class version 1.0")
+	}
+	// Postクラスを元に別のクラスを作成したい場合
+	class SponsoredPost {
+		constructor(text, sponsor) {
+			this.text = text;
+			this.likeCount = 0;	
+			this.sponsor = sponsor;
+			}
+
+			show(){
+				console.log(`${this.text} - ${this.likeCount}like`);
+				console.log(`... sponsord by ${this.sponsor}`);
+			}
+
+			like() {
+				this.likeCount++;
+				this.show();
 			}
 
 	}
@@ -31,12 +42,9 @@
 	const posts = [
 		new Post("JavaScriptの勉強中…"),
 		new Post("プログラミング楽しい！"),
+		new SponsoredPost("3分動画でマスターしよう", "dotinstall"),
 	];
 
-	// 今までメソッドはインスタンスから呼び出しを行っていた
-	// posts[0].like();
-
-	Post.showInfo();
-
-
+	posts[2].show();
+	posts[2].like();
 }
