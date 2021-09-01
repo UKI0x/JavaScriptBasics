d7969e9605c381b515b116b0e47212c7ac008914
UKI0x <ono.oooo.syty@gmail.com>
2021-09-02 00:03:15 +0900

    add クラスを作ってみよう

--- js/main.js
+++ js/main.js
@@ -2,28 +2,22 @@
 
 
 {
-	// オブジェクトが複数ある場合を考える
-	// 匿名で文章を投稿できるSNSを作成したとする
-	// 投稿が複数あるので、まずは配列を作って、その中に文章と言い値を管理するようなオブジェクトを複数入れる
-	const posts = [
-		{
-			text: "JavaScriptの勉強中…",
-			likeCount: 0,
-			// show:function (){
-			// 		console.log(`${this.text} - ${this.likeCount}いいね`);
-			// 	},
-			show(){
-					console.log(`${this.text} - ${this.likeCount}いいね`);
-				},
-				
-			},
-			{
-				text: "プログラミング楽しい！",
-				likeCount: 0,
-				show(){
-						console.log(`${this.text} - ${this.likeCount}いいね`);
-					},
+	//クラスを作成してみる
+	class Post {
+		constructor(text) {
+			this.text = text;
+			this.likeCount = 0;		
 			}
+
+			show(){
+				console.log(`${this.text} - ${this.likeCount}いいね`);
+			}
+	}
+
+
+	const posts = [
+		new Post("JavaScriptの勉強中…"),
+		new Post("プログラミング楽しい！"),
 	];
 
 	// メソッド（オブジェクトの値が関数の時の呼び名）の時の呼び出し方
